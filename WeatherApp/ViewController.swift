3b41a4d345bbbaadaf993ac11d660bc78e4547e0
caiohenriquesilvafranco <caiofranco@skipp.com.br>
2019-11-25 12:11:50 -0300

    Exercicio de revisao da lingugagem Swift

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var bakcgroundView: UIView!
+    
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bakcgroundView.layer.addSublayer(gradientLayer)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setBlueGradientBackground()
+    }
+    
+    func setBlueGradientBackground () {
+        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
+    
+    func setGrayGradientBackgund () {
+        
+    }
+    
+     
 }
 
